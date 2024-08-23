2a8fbbe5bf19debcd3e94153875c40c12c4f32ae
John Zobitz <zobitz@augsburg.edu>
2024-08-23 09:10:13 -0500

    (1) removed duplicate 8250 data files that accumulated in the folders for each consecutive day at a site. (2) removed processing of soil properties in 02-data-prepare (3) reprocessed joined dataset, and (4) computed file for comparison between NEON and field obs.

--- R/process/02-data-prepare.R
+++ R/process/02-data-prepare.R
@@ -1,7 +1,7 @@
 ### Compare LICOR fluxes to model fluxes
 library(tidyverse)
 library(lubridate)
-library(NEONSoils)
+
 
 
 # Load up the flux results
@@ -16,7 +16,7 @@ env_files <- list.files(path='data/raw/flux-data',pattern='env-meas-',full.names
 
 model_fluxes <- vector(mode = "list", length = length(flux_files))
 env_values <- vector(mode = "list", length = length(env_files))
-soil_values <- vector(mode = "list", length = length(env_files))
+
 
 
 for(i in seq_along(model_fluxes)) {
@@ -39,41 +39,6 @@ for(i in seq_along(env_values)) {
   env_values[[i]] <- site_data |> mutate(site=site_name)
 
 
-  # Ingest the megapit soil physical properties pit, horizon, and biogeo data
-  mgp.pit <- site_megapit$mgp_permegapit
-  mgp.hzon <- site_megapit$mgp_perhorizon
-  mgp.bgeo <- site_megapit$mgp_perbiogeosample
-  mgp.bden <- site_megapit$mgp_perbulksample
-
-
-  # Merge the soil properties into a single data frame
-
-  mgp.hzon.bgeo <- inner_join(mgp.hzon, mgp.bgeo, by=c("horizonID", "pitID", "domainID", "siteID", "horizonName"))
-  mgp.hzon.bgeo.bden <- inner_join(mgp.hzon.bgeo, mgp.bden, by=c("horizonID", "pitID", "domainID", "siteID", "horizonName", "labProjID", "laboratoryName"))
-  mgp <- inner_join(mgp.hzon.bgeo.bden, mgp.pit, by=c("pitID", "domainID", "siteID", "pitNamedLocation", "nrcsDescriptionID"))
-
-  ###############################
-  # Future development: Estimate particle density of <2 mm fraction based on Ruhlmann et al. 2006 Geoderma 130,
-  # 272-283. Assumes C content of organic matter is 55%. Constants 1.127, 0.373, 2.684 come
-  # from Ruhlman et al. 2006 (2.684 = particle density of the mineral fraction, "(1.127 +
-  # 0.373*(dfBGChem$Estimated.organic.C..../55))" = particle density of organic matter).
-  ###############################
-
-  # Calculate 2-20 mm rock volume (cm3 cm-3). Assume 2.65 g cm-3 density.
-  rockVol <- ((mgp$coarseFrag2To5 + mgp$coarseFrag5To20) / 1000) / 2.65
-
-  # Calculate porosity of the <2 mm fraction (cm3 cm-3). Assume soil particle density of 2.65 g cm-3.
-  porosSub2mm <- 1 - mgp$bulkDensExclCoarseFrag/2.65
-
-  # Calculate porosity of the 0-20 mm fraction (cm3 cm-3). Assume no pores within rocks.
-  mgp$porVol2To20 <- porosSub2mm * (1 - rockVol)
-
-
-  soil_values[[i]] <- mgp |>
-    filter(biogeoTopDepth == min(biogeoTopDepth,na.rm=TRUE)) |>
-    select(porVol2To20) |>
-    mutate(site = site_name)
-
 }
 
 # Bind everything together
@@ -81,8 +46,6 @@ model_fluxes <- bind_rows(model_fluxes)
 
 env_values <- bind_rows(env_values)
 
-soil_values <- bind_rows(soil_values) |>
-  distinct() # Ok to do this since this is a common measurement
 
 
 
@@ -127,9 +90,11 @@ model_sites <- model_fluxes |>
 env_sites <- env_values |>
   select(-monthly_mean) |>
   filter(measurement %in% c("VSWC","soilTemp")) |>
-  #group_by(site,measurement) |>
-  #nest() |>
-  inner_join(measurement_times,by="site") |>
+  rename(env_data_in = data) |>
+  group_by(site,measurement) |>
+  nest() |>
+  mutate(data = map(.x = data,.f=~(.x |> unnest(cols=c(env_data_in)) |> ungroup()))) |> # Combine consecutive months together
+  inner_join(measurement_times,by="site") |>  # Filter only during times we are at field
   mutate(env_data = pmap(.l=list(data,start_time,end_time,sampling_location),
                            .f=~(..1 |>
                                   ungroup() |>
@@ -151,8 +116,8 @@ env_sites <- env_values |>
 field_data_joined <- model_sites |>
   inner_join(licor_flux,by=c("site")) |>
   inner_join(env_sites,by=c("site","sampling_location")) |>
-  inner_join(soil_values,by="site") |>
-  inner_join(licor_env,by="site")
+  inner_join(licor_env,by="site") |>
+  ungroup()
 
 
 ### Ready to plot fluxes!
---
+++ R/process/03-flux-compare-stats.R
@@ -0,0 +1,102 @@
+### Derive RMSE and R2 values for the different model results compared to measured values
+### Work in progress
+
+library(tidyverse)
+library(lubridate)
+library(broom)
+### Goals:
+# (1) Load up data and measured fluxes for each site
+# (2) Co-locate field obs and neonSoilFlux obs in same half-hourly window
+# (3) Compute R2 and RMSE.  For RMSE we also normalize it by dividing by the mean
+
+# (1) Load up flux data
+load('data/derived/combined-field-data.Rda')
+
+# (2) Co-locate field obs and neonSoilFlux obs in same half-hourly window
+
+standardize_timestamps <- function(input_model_data,input_field_data) {
+
+  # Create a tibble of intervals for NEON
+  start_times <- input_model_data$startDateTime
+  end_times <- input_model_data$startDateTime + 30*60 # Add 30 minutes
+  intervals <- interval(start_times,end_times)
+  dates <- tibble(start_times,intervals)
+
+  # Create a binary variable indicating if any gap-filled environmental data were used to calculate flux
+  model_dataset <- input_model_data |>
+    mutate(fluxMeanQF = if_any(ends_with("MeanQF"), ~ .x == 1))
+
+  # Create a vector of LICOR reported times
+  measurement_times <- input_field_data$startDateTime
+
+
+  # Figure out which NEON intervals the LICOR measured, adding it to the field data
+  my_intervals <- input_field_data |>
+    mutate(ival = intervals[map_int(measurement_times, ~ which(.x %within% intervals))] )
+
+
+  # Join the intervals to the NEON dates so we have a NEON timestamp for each LICOR
+  NEON_field_timestamp <- my_intervals |>
+    group_by(ival) |>
+    nest() |>
+    inner_join(dates,by=c("ival"="intervals")) |>
+    rename(field_flux = data)
+
+  # Join these up now
+  joined_data <- model_dataset |>
+    inner_join(NEON_field_timestamp,by=c("startDateTime" = "start_times")) |>
+    select(startDateTime,flux_compute,field_flux,fluxMeanQF)
+
+  # Prepare a data frame of timestamped NEON and field data for analysis
+
+  out_data <- joined_data |>
+    mutate(field_flux = map(field_flux,~select(.x,-startDateTime))) |>
+    #select(flux_compute,field_flux,fluxMeanQF) |>
+    unnest(cols=c("field_flux")) |>
+    rename(flux_field = flux) |>
+    unnest(cols=c("flux_compute")) |>
+    select(-horizontalPosition) |>
+    ungroup()
+
+
+  return(out_data)
+
+
+
+  }
+
+
+# (3) Compute R2 and RMSE and other regression stats.  For RMSE we also normalize it by dividing by the mean of field obs
+
+extract_stats <- function(harmonized_data) {
+
+  harmonized_data |>
+    group_by(method) |>
+    nest() |>
+    mutate( nrmse = map_dbl(.x=data, .f = ~sqrt(sum((.x$flux-.x$flux_field)^2)/nrow(.x))/mean(.x$flux_field) ),  # computed nrmse - we divide by mean of field obs for standardization
+            lm = map(.x=data,.f=~lm(flux~flux_field,data=.x)),
+            coeff = map(lm,broom::tidy),
+            vals = map(lm,broom::glance),
+            r.squared = map_dbl(.x=vals,.f=~pull(.x,r.squared)),
+            slope = map_dbl(.x=coeff,.f=~pull(.x,estimate)[[2]])) |>
+    select(method,nrmse,r.squared,slope) |>
+    ungroup()
+
+
+}
+
+
+### OK, let's get cooking!
+
+field_stats_data <- field_data_joined |>
+  mutate(harmonized_data = map2(.x=model_data,.y=field_flux,.f=~standardize_timestamps(.x,.y)),
+         comparison_stats = map(harmonized_data,extract_stats)) |>
+  select(site,comparison_stats) |>
+  unnest(cols=c(comparison_stats))
+
+# Plot!
+
+field_stats_data |>
+  pivot_longer(cols=c("nrmse","r.squared","slope")) |>
+  ggplot(aes(x=site,y=value,color=method,group=method)) +
+    geom_point() + geom_line() + facet_grid(name~.,scales="free_y")
